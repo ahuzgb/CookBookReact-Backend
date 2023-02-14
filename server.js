cfd020e6330a7a0b02919704960579e7d1845375
ahuzgb <ahuzgb@gmail.com>
2023-02-14 14:44:02 +0100

    m

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require("express");
-const multer = require("multer");
-const fs = require("fs");
 const app = express();
 const cors = require("cors");
-
 require("dotenv").config();
-const corsOptions = {
-  origin: "https://marvelous-brigadeiros-cbe7ea.netlify.app",
-};
 
-app.use(cors(corsOptions));
-
-app.use("/static", express.static("./uploads"));
-const upload = multer({ dest: "./uploads" });
+app.use(cors());
+app.use(express.json());
 
 const { Pool } = require("pg");
 
@@ -49,34 +41,6 @@ app.post("/api/recipes", (req, res) => {
     .then((data) => res.json(data.rows[0]))
     .catch((e) => res.sendStatus(500));
 });
-
-app.post("/api/recipes", upload.single("selectedFile"), (req, res) => {
-  console.log(req.body);
-
-  const title = req.body.title;
-  const nameid = req.body.nameid;
-  const type = req.body.type;
-  const vegetarian = req.body.vegetarian;
-  const ingredients = req.body.ingredients;
-  const instructions = req.body.instructions;
-
-  const fileType = req.file.mimetype.split("/")[1];
-  const newFile = req.file.filename + "." + fileType;
-  fs.rename(`./uploads/${req.file.filename}`, `./uploads/${newFile}`, () => {
-    console.log("What up");
-  });
-
-  const image = `http://localhost:8060/static/${newFile}`;
-
-  pool
-    .query(
-      "INSERT INTO recipes (title, nameid, image , type , vegetarian  , ingredients , instructions ) VALUES ($1, $2 , $3 , $4 , $5 , $6 , $7) RETURNING * ",
-      [title, nameid, image, type, vegetarian, ingredients, instructions]
-    )
-    .then((data) => res.json(data.rows[0]))
-    .catch((e) => res.sendStatus(500));
-});
-app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
